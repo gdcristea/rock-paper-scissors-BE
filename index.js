83336c3a44f7b2c3d61d18ca848df2e578907836
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2025-04-11 13:03:20 +0300

    added the right domain for cors frontend

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.use(express.json());
  * Need this cors middleware as the frontend and backend run on different domains
  * Frontend - GitHub Pages
  * Backend - Render
- * This middleware adds 'Access-Control-Allow-Origin: https://username.github.io'
+ * This middleware adds 'Access-Control-Allow-Origin: https://gdcristea.github.io'
  * in the responses' header
  */
 app.use(
   cors({
-    origin: "https://username.github.io",
+    origin: "https://gdcristea.github.io",
   })
 );
 
