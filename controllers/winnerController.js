662dc9138f435e6731d54fd00650b21f9bb3fcc9
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2025-04-15 16:40:30 +0300

    added new architecture

---
+++ controllers/winnerController.js
@@ -0,0 +1,28 @@
+const determineWinner = (req, res) => {
+  const { userOption } = req.body;
+  const options = ["rock", "paper", "scissors"];
+  const computerOption = options[Math.floor(Math.random() * options.length)];
+  let result;
+
+  if (userOption === computerOption) {
+    result = "tie";
+  } else if (
+    (userOption === "rock" && computerOption === "scissors") ||
+    (userOption === "scissors" && computerOption === "paper") ||
+    (userOption === "paper" && computerOption === "rock")
+  ) {
+    result = "user";
+  } else {
+    result = "computer";
+  }
+
+  /**
+   * res,json(...):
+   * 1. JSON.stringify()
+   * 2. res.setHeader('Content-Type', 'application/json)
+   * 3. res.send(...)
+   */
+  res.json({ computerOption, result });
+}
+
+module.exports = determineWinner;
