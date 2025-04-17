4b65b473e2bac57b0221e5bd177ff3f0a3b6d2aa
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2025-04-17 17:27:42 +0300

    updated yarn install command to also work with devDependencies

--- yarn-install-clean.sh
+++ yarn-install-clean.sh
@@ -3,6 +3,17 @@
 # Disable proxies
 unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
 
-#  Forward all arguments to yarn
-yarn "$@"
+# Extract and clean --dev or -D from arguments
+ARGS=()
+DEV_FLAG=""
 
+for arg in "$@"; do
+  if [[ "$arg" == "--dev" || "$arg" == "-D" ]]; then
+    DEV_FLAG="--dev"
+  else
+    ARGS+=("$arg")
+  fi
+done
+
+# Run yarn with or without --dev flag
+yarn add "${ARGS[@]}" $DEV_FLAG
