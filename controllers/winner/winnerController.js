d42d7ec82371fa78289705a1cd13d3d272663b29
georgedaniel.cristea <georgedaniel.cristea@bcr.ro>
2025-04-19 13:19:54 +0300

    added documentation for winner controller

--- controllers/winner/winnerController.js
+++ controllers/winner/winnerController.js
@@ -1,3 +1,38 @@
+/**
+ * @function determineWinner
+ * @description
+ * This controller determines the result of a Rock-Paper-Scissors game round.
+ * It compares the user's selected option against a randomly selected option for the computer.
+ * Based on the rules of the game, it returns the result as either:
+ *  - "tie"
+ *  - "user" (user wins)
+ *  - "computer" (computer wins)
+ *
+ * @route POST /api/winner
+ * @access Public
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} req.body - Request body should contain `userOption`
+ * @param {string} req.body.userOption - User's selected move: "rock", "paper", or "scissors"
+ *
+ * @param {Object} res - Express response object
+ * @returns {Object} JSON response with:
+ *  - {string} computerOption - Randomly chosen move by the computer
+ *  - {string} result - "tie" | "user" | "computer"
+ *
+ * @example
+ * // Request
+ * POST /api/winner
+ * {
+ *   "userOption": "rock"
+ * }
+ *
+ * // Response
+ * {
+ *   "computerOption": "scissors",
+ *   "result": "user"
+ * }
+ */
 exports.determineWinner = (req, res) => {
   const { userOption } = req.body;
   const options = ["rock", "paper", "scissors"];
@@ -23,4 +58,4 @@ exports.determineWinner = (req, res) => {
    * 3. res.send(...)
    */
   res.json({ computerOption, result });
-}
+};
